b79fdf33a8811fb5ad99ff6e2ef996f9ceb6104a
Mihir Kumar <mihirpandey.13@gmail.com>
2023-03-27 00:00:00 +0530

    code part-1 updated

--- 3-event-loop/code/demo-2.js
+++ 3-event-loop/code/demo-2.js
@@ -1,20 +1,20 @@
 import { readFile } from 'fs';
 
-console.log('Start')
+console.log(`Start`)
 
 setTimeout(() => {
-    console.log(`Outside Timeout callback executed`)
+    console.log(`Outside Timeout callback executed - 1st`)
 }, 10)
 
-readFile('./data.txt', 'utf-8', (err, data) => {
-    console.log('File I/O callback')
+readFile(`./data.txt`, `utf-8`, (err, data) => {
+    console.log(`File I/O callback`)
 
     setTimeout(() => {
-        console.log('Timeout callback executed')
+        console.log(`Timeout callback executed`)
     }, 0)
 
     setImmediate(() => {
-        console.log('Immediate callback executed')
+        console.log(`Immediate callback executed`)
     })
 
     if (err) {
@@ -24,4 +24,8 @@ readFile('./data.txt', 'utf-8', (err, data) => {
     }
 })
 
-console.log('End')
+setTimeout(() => {
+    console.log(`Outside Timeout callback executed - 2nd`)
+}, 0)
+
+console.log(`End`)
